cfd6e2da89580aa7a2d3b6c855e54194e13cf996
EC2 Default User <ec2-user@ip-172-31-15-150.ap-northeast-1.compute.internal>
2021-03-08 03:33:48 +0000

    for test

---
+++ udemy.rb
@@ -0,0 +1,26 @@
+# a = 123
+# b = "The number is #{a}"
+
+# puts b
+
+# method定義
+# def method_a
+#     puts "This is method a!"
+# end
+
+# method_a 
+
+#配列
+# a = [1,2,3]
+
+# puts a[0]
+# puts a[1]
+# puts a[2]
+
+# puts a
+
+h = {"a" => 123, "b" => 456, "c" => 789}
+
+puts h["a"]
+puts h["b"]
+puts h["c"]
